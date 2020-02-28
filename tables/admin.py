8fad535ae2b70d1770f6c9c5e4899b562622ad7e
Tai Doan <doantai11@gmail.com>
2020-02-27 21:21:55 -0800

    Remove tables

--- tables/admin.py
+++ tables/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from .models import Address, Item, Customer
+
 # Register your models here.
 admin.site.register(Address)
 admin.site.register(Item)
--- tables/views.py
+++ tables/views.py
@@ -1,13 +1,10 @@
 from django.shortcuts import render
-from .models import Address, Item, Customer, Payment, Food_Style
-
+from .models import Address, Item, Customer
 # Create your views here.
 def home(request):
     addresses = Address.objects
     items = Item.objects
     customers = Customer.objects
-    payments = Payment.objects
-    food_styles = Food_Style.objects
     return render(request, 'tables/home.html',{
     'addresses': addresses,
     'items': items,

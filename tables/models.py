40f2ddb40cc1128e4974963b8324351e5444f15c
Sebastian Hernandez <33647914+seabass189@users.noreply.github.com>
2020-03-09 15:51:31 -0700

    the first commit

---
+++ tables/models.py
@@ -0,0 +1,120 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+class Address(models.Model):
+    street = models.CharField(max_length=50)
+    city = models.CharField(max_length=50)
+    state = models.CharField(max_length=50)
+    zipcode = models.IntegerField()
+
+    def __str__(self):
+        return self.street + " " + self.city
+
+class Item(models.Model):
+    name = models.CharField(max_length=50)
+    price = models.DecimalField(decimal_places=2,max_digits=8)
+    menu_id = models.IntegerField()
+    is_spicy = models.BooleanField()
+
+    def __str__(self):
+        return self.name + " from menu: " + str(self.menu_id)
+
+class Customer(models.Model):
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    phone_number = models.CharField(max_length=20, default='')
+    stripeid = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.first_name + ' ' + self.last_name
+
+class Customer_Address(models.Model):
+    address_id = models.IntegerField()
+    customer_id = models.IntegerField()
+
+    def __str__(self):
+        return 'Add. Id: ' + str(self.address_id) + ' Customer Id: ' + str(self.customer_id)
+
+class Customer_Payment(models.Model):
+    payment_id = models.IntegerField()
+    customer_id = models.IntegerField()
+
+    def __str__(self):
+        return 'Payment Id: ' + str(self.payment_id) + ' Customer Id: ' + str(self.customer_id)
+
+class Customer_Style_Preference(models.Model):
+    style_id = models.IntegerField()
+    customer_id = models.IntegerField()
+
+    def __str__(self):
+        return 'Style Id: ' + str(self.style_id) + ' Customer Id: ' + str(self.customer_id)
+
+class Food_Style(models.Model):
+    style_name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.style_name
+
+class Restaurant(models.Model):
+    #name
+    name = models.CharField(max_length=50)
+    #address
+    address = models.CharField(max_length=50)
+    #phone_number
+    #max length on the table is 50 but i figure we should change this later
+    phone_number = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name + ' ' + self.address
+
+class Restaurant_Style(models.Model):
+    #restaurant_id
+    restaurant_id = models.IntegerField()
+    #style_id
+    style_id = models.IntegerField()
+
+    def __str__(self):
+        return 'Rest. Id: ' + str(self.restaurant_id) + ' Style Id:' + str(self.style_id)
+
+class Review(models.Model):
+    #text
+    text = models.CharField(max_length=300)
+    #stars
+    stars = models.PositiveSmallIntegerField()
+    #customer_id
+    customer_id = models.IntegerField()
+    #restaurant_id
+    restaurant_id = models.IntegerField()
+
+    def __str__(self):
+        return 'Customer Id: ' + str(self.customer_id) + ' Rest. Id: '
+        + str(self.restaurant_id) + ' ' + str(self.stars) + " stars"
+
+class Item_Style(models.Model):
+    item_id = models.IntegerField()
+    style_id = models.IntegerField()
+
+    def __str__(self):
+        return 'Item Id: ' + str(self.item_id) + ' Style. Id: '
+        + str(self.style_id)
+
+class Menu(models.Model):
+    menu_id = models.IntegerField()
+    restaurant_id = models.IntegerField()
+
+    def __str__(self):
+        return 'Menu Id: ' + str(self.menu_id) + ' Rest. Id: '
+        + str(self.restaurant_id)
+
+class Payment (models.Model):
+    #cardnumber
+    card_number = models.BigIntegerField()
+    #pin number
+    card_pin = models.IntegerField()
+    #Expiration Date
+    card_expdate = models.DateField()
+
+    def __str__(self):
+        return 'Card Num: ' + str(self.card_number)
---
+++ tables/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from .forms import CustomSignupForm
+from django.urls import reverse_lazy
+from django.views import generic
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required, user_passes_test
+import stripe
+from decouple import config
+
+from .models import Restaurant
+
+stripe.api_key = config['STRIPE_API_KEY']
+
+# Create your views here.
+def home(request):
+    restaurants = Restaurant.objects
+    return render(request, 'tables/home.html', {'restaurants':restaurants})
+
+class SignUp(generic.CreateView):
+    form_class = CustomSignupForm
+    success_url = reverse_lazy('home')
+    template_name = 'registration/signup.html'
+
+    def form_valid(self, form):
+        valid = super(SignUp, self).form_valid(form)
+        username, password = form.cleaned_data.get('username'), form.cleaned_data.get('password1')
+        new_user = authenticate(username=username, password=password)
+        login(self.request, new_user)
+        return valid
+
+def join(request):
+    return render(request, 'tables/join.html')

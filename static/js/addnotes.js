7e18172caba3ef241e9726cb118df183ee8d6d9f
Vy Nguyen <marvin.calvin.1705@gmail.com>
2020-04-19 22:55:17 -0700

    move cart button to top

--- static/js/addnotes.js
+++ static/js/addnotes.js
@@ -2,3 +2,9 @@ function clickAddNote() {
   document.getElementById("textboxid").className="show";
   document.getElementById("hrefid").className="hide";
 }
+function enterNote() {}
+function clickAddInstructionNote() {
+  document.getElementById("textbox").className="show";
+  document.getElementById("href").className="hide";
+}
+function enterInstructionNote() {}

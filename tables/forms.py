81227c7fd13d2705a8c3c7a06e7bd688d0b4e912
Sebastian Hernandez <33647914+seabass189@users.noreply.github.com>
2020-04-23 12:03:44 -0700

    Got rid of name checkout form

--- tables/forms.py
+++ tables/forms.py
@@ -23,10 +23,12 @@ class AddressForm(forms.ModelForm):
 class CheckoutForm(forms.Form):
     shipping_address = forms.CharField(required=False)
     shipping_address2 = forms.CharField(required=False)
+    shipping_state = forms.CharField(label='State', max_length=50)
     shipping_zip = forms.CharField(required=False)
 
     billing_address = forms.CharField(required=False)
     billing_address2 = forms.CharField(required=False)
+    billing_state = forms.CharField(label='State', max_length=50)
     billing_zip = forms.CharField(required=False)
 
     same_billing_address = forms.BooleanField(required=False)
@@ -37,3 +39,16 @@ class CheckoutForm(forms.Form):
 
     # payment_option = forms.ChoiceField(
     #     widget=forms.RadioSelect, choices=PAYMENT_CHOICES)
+
+
+class BillingCheckout(forms.Form):
+    billing_street = forms.CharField(label='Address', max_length=50)
+    billing_city = forms.CharField(label='City', max_length=50)
+    billing_state = forms.CharField(label='State', max_length=50)
+    billing_zipcode = forms.CharField(label='Zipcode', max_length=15)
+
+class DeliveryCheckout(forms.Form):
+    delivery_street = forms.CharField(label='Address', max_length=50)
+    delivery_city = forms.CharField(label='City', max_length=50)
+    delivery_state = forms.CharField(label='State', max_length=50)
+    delivery_zipcode = forms.CharField(label='Zipcode', max_length=15)

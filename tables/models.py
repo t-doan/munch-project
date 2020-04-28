c11123cd8a4d37caf2be7d5a5bc3a3a9417c2dbb
Sebastian Hernandez <33647914+seabass189@users.noreply.github.com>
2020-04-28 15:11:21 -0700

    Bugs and Stripe integration started
    Fixed issue with empty cart, plus started adding Stripe integration to 
    the models

--- tables/models.py
+++ tables/models.py
@@ -51,7 +51,7 @@ class Customer(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     phone_number = models.CharField(max_length=20, default='')
-    stripeid = models.CharField(max_length=255)
+    stripeid = models.CharField(max_length=255, blank=True, null=True)
 
     def __str__(self):
         return self.first_name + ' ' + self.last_name
